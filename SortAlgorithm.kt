e2b4263d7220bff007cb7263fe70b131e9f30bac
gmfonseca <gmagnofonseca@gmail.com>
2021-02-27 19:43:12 -0300

    Remove fun interface (requires higher kotlin version)

--- SortAlgorithm.kt
+++ SortAlgorithm.kt
@@ -3,7 +3,7 @@ package br.com.gmfonseca.tcc.algorithms
 /**
  * A generic collections sorter algorithm. Receives comparable elements, making sort easy.
  */
-fun interface SortAlgorithm<T : Comparable<T>> {
+interface SortAlgorithm<T : Comparable<T>> {
 
     fun sort(items: List<T>): List<T>
 
